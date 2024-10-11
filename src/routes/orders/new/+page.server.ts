a0922347e4bd44ea6f2841dbc3af04497032755e
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2024-10-11 06:03:33 +0200

    basic order and trip forms

--- src/routes/orders/new/+page.server.ts
+++ src/routes/orders/new/+page.server.ts
@@ -1,16 +1,27 @@
-import { fail } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import { setError, superValidate, message } from 'sveltekit-superforms/server';
 import { setFlash } from 'sveltekit-flash-message/server';
 import type { Actions } from "@sveltejs/kit";
 import { orderSchema } from '$lib/config/zod-schemas.js';
 import { createOrder, createOrderItem } from '$lib/server/database/order-model.js';
-import type { Order } from '$lib/server/database/drizzle-schemas.js';
+import type { Order, Trip } from '$lib/server/database/drizzle-schemas.js';
+import { getTripsByUserId } from '$lib/server/database/trips-model.js';
 
 
- 
+
 export const load = async (event) => {
+  const user = event.locals.user;
+  let trips: Trip[] = [];
+  if (!user) {
+    redirect(302, '/auth/sign-in');
+  }
+
+  // Guests should be able to see their orders only
+  trips = await getTripsByUserId(user.id)
+
   return {
     form: await superValidate(event, orderSchema),
+    trips
   };
 };
 
@@ -25,34 +36,34 @@ export const actions: Actions = {
     }
 
     try {
-        console.log('creating new order')
-        const user = event.locals.user
-        if (user) {
-            const order = await createOrder({
-                id: crypto.randomUUID(),
-				tripId: form.data.tripId,
-                userEmail: form.data.userEmail,
-				currency: form.data.currency,
-				totalPrice: 0,
-                createdAt: new Date(),
-                updatedAt: new Date()
+      console.log('creating new order')
+      const user = event.locals.user
+      if (user) {
+        const order = await createOrder({
+          id: crypto.randomUUID(),
+          tripId: form.data.tripId,
+          userEmail: form.data.userEmail,
+          currency: form.data.currency,
+          totalPrice: 0,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+        if (order) {
+          form.data.items.forEach(async (orderItem) => {
+            await createOrderItem(order.id, {
+              currency: orderItem.currency,
+              description: orderItem.description,
+              quantity: orderItem.quantity,
+              unitPrice: orderItem.unitPrice
             })
-			if (order) {
-				form.data.items.forEach(async (orderItem) => {
-					await createOrderItem(order.id, {
-						currency: orderItem.currency,
-						description: orderItem.description,
-						quantity: orderItem.quantity,
-						unitPrice: orderItem.unitPrice
-					})
-				});
-			}
-			
-            setFlash({ type: 'success', message: 'Order create successful.' }, event);
+          });
         }
+
+        setFlash({ type: 'success', message: 'Order create successful.' }, event);
+      }
     } catch (e) {
-        console.error(e);
-        return setError(form, 'There was a problem creating the order.');
+      console.error(e);
+      return setError(form, 'There was a problem creating the order.');
     }
     console.log('order created successfully');
     return message(form, 'Order created successfully.');
--- src/routes/orders/new/+page.svelte
+++ src/routes/orders/new/+page.svelte
@@ -1,8 +1,85 @@
 <script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import * as Alert from '$lib/components/ui/alert';
+	import { orderSchema, type OrderSchema } from '$lib/config/zod-schemas';
+	import type { SuperValidated } from 'sveltekit-superforms';
+	import { Loader2 } from 'lucide-svelte';
+	import { AlertCircle } from 'lucide-svelte';
+	import { Select, SelectGroup, SelectValue } from '$lib/components/ui/select';
+	import SelectTrigger from '$lib/components/ui/select/select-trigger.svelte';
+	import SelectContent from '$lib/components/ui/select/select-content.svelte';
+	import SelectLabel from '$lib/components/ui/select/select-label.svelte';
+	import SelectItem from '$lib/components/ui/select/select-item.svelte';
+	export let data: any;
 
+	export let form: SuperValidated<OrderSchema>;
+	export let trips;
+	trips = data.trips;
+	form = data.form;
 </script>
 
-<section class="container grid items-center gap-6">
-    <p> new order page </p>
-	
-</section>
+<div class="flex items-center justify-center mx-auto max-w-2xl">
+	<Form.Root let:submitting let:errors method="POST" {form} schema={orderSchema} let:config>
+		<Card.Root>
+			<Card.Header class="space-y-1">
+				<Card.Title class="text-2xl">Order</Card.Title>
+				<Card.Description>Create an order</Card.Description>
+			</Card.Header>
+			<Card.Content class="grid gap-4">
+				{#if errors?._errors?.length}
+					<Alert.Root variant="destructive">
+						<AlertCircle class="h-4 w-4" />
+						<Alert.Title>Error</Alert.Title>
+						<Alert.Description>
+							{#each errors._errors as error}
+								{error}
+							{/each}
+						</Alert.Description>
+					</Alert.Root>
+				{/if}
+				<Select>
+					<SelectTrigger>
+						<SelectValue placeholder="Select a trip" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>Trips</SelectLabel>
+							{#each trips as trip}
+								<SelectItem value={trip.id}>{trip.origin} to {trip.destination}</SelectItem>
+							{/each}
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+				<Form.Button class="w-full" href="/trips/new"
+						>{#if submitting}
+							<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+							Please wait{:else}+ New trip{/if}
+					</Form.Button>
+
+				<Form.Field {config} name="userEmail">
+					<Form.Item>
+						<Form.Label>User Email</Form.Label>
+						<Form.Input />
+						<Form.Validation />
+					</Form.Item>
+				</Form.Field>
+				<Form.Button class="w-full" disabled={submitting}
+					>{#if submitting}
+						<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+						Please wait{:else}+ Add item{/if}
+				</Form.Button>
+			</Card.Content>
+			<Card.Footer>
+				<div class="block w-full">
+					<Form.Button class="w-full" disabled={submitting}
+						>{#if submitting}
+							<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+							Please wait{:else}Register order{/if}
+					</Form.Button>
+				</div>
+			</Card.Footer>
+		</Card.Root>
+	</Form.Root>
+</div>
